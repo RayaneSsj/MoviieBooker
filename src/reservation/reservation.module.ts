3e86aff32e6af9d3ffcd34c03c70a94afb920227
rayane rostane <rayane.rostane@universite-paris-saclay.fr>
2025-02-06 14:58:48 +0100

    Suppression des fichiers inutiles, ne gardant que backend

--- src/reservation/reservation.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReservationService } from './reservation.service';
-import { ReservationController } from './reservation.controller';
-
-@Module({
-  providers: [ReservationService],
-  controllers: [ReservationController]
-})
-export class ReservationModule {}
